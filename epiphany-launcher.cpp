6e59cb26ad3ac315d2c9547fb44d51900044151b
Nenad Miksa <nenad.miksa@microblink.com>
2021-10-21 20:23:52 +0200

    I'm desperate

--- epiphany-launcher.cpp
+++ epiphany-launcher.cpp
@@ -1,6 +1,8 @@
 #include <cstdlib>
 #include <string>
 
+#include <unistd.h>
+
 int main( int argc, char * argv[] )
 {
     std::string cmdline{ "flatpak run org.gnome.Epiphany" };
@@ -9,5 +11,6 @@ int main( int argc, char * argv[] )
         cmdline += ' ';
         cmdline += argv[ i ];
     }
+	sleep( 1 ); // wait 1 second because dbus is slow in dying, which causes problems when launching the next instance of Epiphany
     return system( cmdline.c_str() );
 }
